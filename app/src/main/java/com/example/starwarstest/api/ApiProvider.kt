20d7ec7f53275d9148621ebb00233c33a61c42b4
HardcatAntoine <116114622+HardcatAntoine@users.noreply.github.com>
2023-08-03 12:17:09 +0300

    api

---
+++ app/src/main/java/com/example/starwarstest/api/ApiProvider.kt
@@ -0,0 +1,28 @@
+package com.example.starwarstest.api
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object ApiProvider {
+    const val BASE_URL = "https://swapi.dev/"
+    val pages = 1..42
+    private val gsonConverter = GsonConverterFactory.create()
+    private val loggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    private val httpClient = OkHttpClient
+        .Builder()
+        .addInterceptor(loggingInterceptor)
+        .build()
+    private val retrofit = Retrofit
+        .Builder()
+        .baseUrl(BASE_URL)
+        .client(httpClient)
+        .addConverterFactory(gsonConverter)
+        .build()
+    private val api = retrofit.create(ApiService::class.java)
+    val apiService: ApiService
+        get() = api
+}
